982f169251ff994143366fd4d5a47170f86f019b
aphsai <saksham.a@hotmail.ca>
2018-01-31 17:01:56 -0500

    added spin, press shift

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ window.onload = function createWorld() {
   canvas.height = window.innerHeight;
   window.addEventListener("keyup", keyup, false);
   window.addEventListener("keydown", keydown, false);
+
   const RUNNING_FRAMES = 12;
   const STANDING_FRAMES = 1;
+  const SPINNING_FRAMES = 7;
   const SPRITE_WIDTH = 300;
   const SPRITE_HEIGHT = 300;
 
@@ -19,20 +21,29 @@ window.onload = function createWorld() {
 
   var running = [];
   var standing = [];
+  var spinning = [];
+
   var frameCounter = 11;
+  var spinCounter = 0;
   var movingRight = false;
   var movingLeft = false;
-  var moving = false;
+  var spin = false;
+  var power = 0;
   var facing_right = true;
   var sprite_x = -SPRITE_WIDTH/2;
   var sprite_y = -SPRITE_HEIGHT/2;
   var sprite_rel_x = 0;
   var sprite_rel_x = 0;
+
   ct.translate(canvas.width/2 , canvas.height/2);
 
   for (var x = 0; x < RUNNING_FRAMES; x++) {
     running[x] = new Image();
     running[x].src = "Animations/Running" + (x + 1) + ".png";
+  }
+  for (var x = 0; x < SPINNING_FRAMES; x++) {
+    spinning[x] = new Image();
+    spinning[x].src = "Animations/Spinning" + (x + 1) + ".png";
   }
   for (var x = 0; x < STANDING_FRAMES; x++) {
     standing[x] = new Image();
@@ -50,6 +61,11 @@ window.onload = function createWorld() {
     movingLeft = false;
       if(e.keyCode == RIGHT)
     movingRight = false;
+      if(e.keyCode == SHIFT) {
+        spinCounter = 0 ;
+        spin = false;
+        power = 0;
+    }
   }
   function keydown(e) {
     if(e.keyCode == LEFT) {
@@ -71,7 +87,11 @@ window.onload = function createWorld() {
       }
       facing_right = true;
     }
-    moving = true;
+    if (e.keyCode == SHIFT) {
+      movingLeft = false;
+      movingRight = false;
+      spin = true;
+    }
   }
   function update() {
     clear();
@@ -82,10 +102,17 @@ window.onload = function createWorld() {
     if (movingRight) sprite_rel_x -= 10;
     ct.fillStyle = "#0b532e";
     ct.fillRect(sprite_rel_x, 95, 50, 50);
-    if (moving && (movingLeft || movingRight)) {
+    if (movingLeft || movingRight) {
       frameCounter += 1;
       frameCounter %= running.length;
-      ct.drawImage(running[frameCounter], sprite_y, sprite_y, SPRITE_WIDTH, SPRITE_HEIGHT);
+      ct.drawImage(running[frameCounter], sprite_x, sprite_y, SPRITE_WIDTH, SPRITE_HEIGHT);
+    }
+    else if (spin) {
+      spinCounter += 1;
+      power += 1;
+      sprite_rel_x -= 20;
+      if (spinCounter >= spinning.length) spinCounter = 4;
+      ct.drawImage(spinning[spinCounter], sprite_x, sprite_y, SPRITE_WIDTH, SPRITE_HEIGHT);
     }
     else {
       frameCounter = 10;
